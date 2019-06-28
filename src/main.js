8b7a386510363c067ed65168ccf9bc9c6a735780
jleeson <jleeson2000@gmail.com>
2019-06-28 14:04:32 -0700

    Added Rects

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@
 //Graphics
 export { makeDisplay, make2DContextCurrent, resizeDisplay, appendDisplay, clearDisplay } from "./graphics/Display";
 export { makeActor, drawActor } from "./objects/Actor";
+export { makeRect, doRectsCollide } from "./physics/Rect";
---
+++ src/physics/Rect.js
@@ -0,0 +1,30 @@
+/**
+ * creates a rect object
+ * @param {Number} x - x position of the rect
+ * @param {Number} y - y position of the rect
+ * @param {Number} width - width of the rect
+ * @param {Number} height - height of the rect
+ * @return {Object}
+ */
+export function makeRect(x, y, width, height) {
+    return {
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        right: x + width,
+        bottom: y + height,
+    };
+}
+
+/**
+ * checks if 2 rects are colliding
+ * @param {Object} rect1 - first rectangle
+ * @param {Object} rect2 - second rectangle
+ */
+export function doRectsCollide(rect1, rect2) {
+    return (rect1.x < rect2.right &&
+        rect2.x < rect1.right &&
+        rect1.y < rect2.bottom &&
+        rect2.y < rect1.bottom);
+}
